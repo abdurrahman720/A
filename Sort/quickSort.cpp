623b473018a2a1b59df06efaf56ae4bd1a2a93a6
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-11-13 02:37:09 +0600

    quicksort

--- Sort/quickSort.cpp
+++ Sort/quickSort.cpp
@@ -20,6 +20,35 @@ void printArr( int arr[], int n){
              cout<<endl;
 }
 
+int partition( int arr[], int l, int h){
+    int pivot = arr[l];
+    int start = l;
+    int end = h;
+
+  while(start < end){
+      while(arr[start] <= pivot){
+        start++;
+    }
+    while(arr[end] >= pivot){
+        end--;
+    }
+    if(start<end){
+       swap(arr[start],arr[end]);
+    }
+  }
+  swap(arr[l],arr[end]);
+
+  return end;
+}
+
+void quickSort(int arr[], int l, int h){
+        if(l<h){
+            int loc = partition(arr,l,h); //get new pivot element from partition
+            quickSort(arr, l, loc - 1);
+            quickSort(arr, loc + 1, h);
+        }
+}
+
 int main(){
     int n;
     cout << "Enter the array size:" << endl;
@@ -31,7 +60,11 @@ int main(){
     printArr(array, n);
 
 
+ quickSort(array, 0, n - 1);
+    cout << "Array after sorted: " << endl;
+    printArr(array, n);
 
+    delete[] array;
 
     return 0;
 }
